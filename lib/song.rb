493d539324f3d333ff60b836c4ce18656f7453f5
Celina Jackson <celinanjackson@gmail.com>
2020-07-08 19:01:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,8 +4,14 @@ class Song
   @@count = 0
   @@genres = [ ]
   @@artists = [ ]
-  def initialize
+
+  def initialize(song_name, artist, genre)
+    @name = song_name 
+    @artist = artist 
+    @genre = genre 
     @@count += 1
+    @@genres << genre 
+    @@artists << artist 
   end
   def self.count
     @@count
