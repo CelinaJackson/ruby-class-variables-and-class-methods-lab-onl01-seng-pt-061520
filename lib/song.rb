215528329fc99720db3480818c689c2cb73aa28c
Celina Jackson <celinanjackson@gmail.com>
2020-07-08 19:13:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,16 @@ class Song
   def self.artists
     @@artists.uniq!
   end
-
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+  end
+  
 
 
 
